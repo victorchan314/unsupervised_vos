e48d4b10b97b6fa9d471b5f6b061df041f6adefc
Victor Chan <victorchan88888888@gmail.com>
2020-05-15 02:50:29 -0700

    Add val sequence to eval_view

--- davis/python/tools/eval_view.py
+++ davis/python/tools/eval_view.py
@@ -36,8 +36,8 @@ def parse_args():
 	parser.add_argument(dest='input',default=None,type=str,
 			help='Path to the HDF5 evaluation file to be displayed.')
 
-	parser.add_argument('--eval_set',default='all',type=str,
-			choices=['training','test','all'],help='Select set of videos to evaluate.')
+	parser.add_argument('--eval_set',default='val',type=str,
+			choices=['train','val','trainval'],help='Select set of videos to evaluate.')
 
 	parser.add_argument('--summary',action='store_true',
 			help='Print dataset average instead of per-sequence results.')
@@ -54,8 +54,15 @@ if __name__ == '__main__':
 
 	technique = osp.splitext(osp.basename(args.input))[0]
 
+        sequence = None
+        if args.eval_set == "val":
+            sequence = ['blackswan', 'bmx-trees', 'breakdance', 'camel', 'car-roundabout', 'car-shadow', 'cows', 'dance-twirl', 'dog', 'drift-chicane', 'drift-straight', 'goat', 'horsejump-high', 'kite-surf', 'libby', 'motocross-jump', 'paragliding-launch', 'parkour', 'scooter-black', 'soapbox']
+        else:
+            raise ValueError("Eval set must be val, not {}".format(args.eval_set))
+
 	db_eval_dict = db_read_eval(technique,raw_eval=False,
-			inputdir=osp.dirname(args.input))
+			inputdir=osp.dirname(args.input),
+                        sequence=sequence)
 
 	log.info("Displaying evaluation of: %s"%osp.basename(args.input))
 
