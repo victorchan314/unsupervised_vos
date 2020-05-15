cb8067dae596833f9c598ce70bac4c311e1ce66e
Victor Chan <victorchan88888888@gmail.com>
2020-05-15 04:39:00 -0700

    Temp

--- davis/python/lib/davis/dataset/loader.py
+++ davis/python/lib/davis/dataset/loader.py
@@ -238,8 +238,8 @@ class DAVISAnnotationLoader(DAVISSegmentationLoader):
 				cfg,sequence,None,ext_im,ext_an,load_func)
 
 	def _eval(self,db_segmentation,eval_func,measure,scale=1):
-                print(1/0)
 		annotations = self._masks[1:-1]
+                print(1/0)
 
 		# Strip of first and last frame if available
 		segmentation = db_segmentation._masks[
